2e28dc397d83696ed45a3dd4b95b05a6e24cdeec
fatihcam46 <fatih.cam.bahar@gmail.com>
2022-08-11 13:29:16 +0100

    added testing test1

--- src/testCases/day4_testCase1.java
+++ src/testCases/day4_testCase1.java
@@ -4,7 +4,8 @@ public class day4_testCase1 {
     public static void main(String[] args) {
         //testing some thing
         //more testing
-        //
+        //negative teting
+        //positive testing
 
     }
 }
